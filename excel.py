f26769313788e7b663d0b76a810fdc7c598f66ad
jiho <search5@gmail.com>
2018-12-12 19:28:38 +0900

    부품 중복 걸러내는 중

--- excel.py
+++ excel.py
@@ -1,4 +1,7 @@
 #-*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import absolute_import
+
 __author__ = 'jiho'
 
 """
@@ -20,8 +23,9 @@ Workbook/
 """
 
 import xml.etree.cElementTree as ET
-from style import Style
-from table import RowMapper
+from .style import Style
+from .table import RowMapper
+
 
 """
 # 최종목적은 row[0]['column'].style.getUnderline()과 같은 형태를 원하는 것임..
@@ -34,6 +38,8 @@ Examples:
 
     >>> print rows[0]['기록철'].style.getUnderline()
 """
+
+
 class ExcelLib:
     def __init__(self, parse_xml_name):
         self.xml = ET.parse(parse_xml_name)
@@ -43,51 +49,75 @@ class ExcelLib:
         }
         self.styles = {}
         self.rows = []
+        self.column_header = None
+        self.sheet = None
+        self.header_line = 0
+        self.content_line = 1
+
+    def set_header_line(self, num=0):
+        self.header_line = num
+
+    def set_content_line(self, num=1):
+        self.content_line = num
 
     # Worksheet 선택 메서드
-    def setWorksheet(self, sheet_name):
-        self.sheet = None
-
-        for worksheet in self.xml.findall("./{%s}Worksheet" % (self.xmlns)):
+    def set_worksheet(self, sheet_name):
+        for worksheet in self.xml.findall("./{%s}Worksheet" % self.xmlns):
             if worksheet.get('{%s}Name' % self.namespaces['ss']) == sheet_name:
                 self.sheet = worksheet
 
-        if self.sheet == None:
+        if not self.sheet:
             raise Exception('%s 이름을 가진 워크시트가 없습니다' % sheet_name)
 
-    # Style 구축 메서드
-    def buildStyles(self):
-        styles = self.xml.find("./{%s}Styles" % self.xmlns)
-        for style in styles.findall('./{%s}Style' % self.xmlns):
-            objStyle = Style(style)
+    def get_sheets(self):
+        sheets = [worksheet.get('{%s}Name' % self.namespaces['ss'])
+                  for worksheet in self.xml.findall("./{%s}Worksheet" % self.xmlns)]
+        return sheets
 
-            self.styles[objStyle.styleID] = objStyle
+    # Style 구축 메서드
+    def build_styles(self):
+        styles = self.xml.find("./{%s}Styles" % self.xmlns)
+
+        for style in styles.findall('./{%s}Style' % self.xmlns):
+            obj_style = Style(style)
+
+            self.styles[obj_style.styleID] = obj_style
 
     # 로우 정보 구축하기
-    def buildRows(self):
+    def build_rows(self):
+        if self.header_line < -1:
+            raise ExcelLibException('헤더 라인 번호가 입력되어 있지 않습니다')
+
+        if not self.content_line:
+            raise ExcelLibException('콘텐츠 라인 시작 번호가 입력되어 있지 않습니다')
+
         table = self.sheet.find("./{%s}Table" % self.xmlns)
         rows = table.findall("./{%s}Row" % self.xmlns)
 
         build_rows = []
 
         # 헤더라인을 미리 가져온다(컬럼명 라인 - 사용자가 입력한)
-        row_header = rows[0]
+        row_header = rows[self.header_line]
 
-        for i, row in enumerate(rows[2:]):
+        for i, row in enumerate(rows[self.content_line:]):
             data_row_meta = RowMapper(row_header, row, self.styles)
             build_rows.append(data_row_meta)
 
-        self.column_header = build_rows[0].keys()
+        self.column_header = tuple(build_rows[self.header_line].keys())
 
         return build_rows
 
-    def getColumnHeader(self):
+    def get_column_header(self):
         return self.column_header.keys()
+
+
+class ExcelLibException(Exception):
+    pass
 
 
 if __name__ == "__main__":
     obj = ExcelLib(u'제5차국내외문서목록(20130917)테스트.xml')
-    obj.setWorksheet(u'2011 국내문서-낱장')
-    obj.buildStyles()
-    obj.buildRows()
-    print obj.getColumnHeader()
+    obj.set_worksheet(u'2011 국내문서-낱장')
+    obj.build_styles()
+    obj.build_rows()
+    print(obj.get_column_header())
--- table.py
+++ table.py
@@ -1,20 +1,21 @@
 #-*- coding: utf-8 -*-
 from __future__ import print_function
 import datetime
-from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, tostring, register_namespace
 import copy
 
 __author__ = 'jiho'
 
 
 class RowMapper:
-    def __init__(self, headers, row = None, styles = None):
+    def __init__(self, headers, row=None, styles=None):
         self.xmlns = 'urn:schemas-microsoft-com:office:spreadsheet'
         self.columns = {}
 
         self.cells = None
 
-        if row != None: # 실 데이터가 넘어오지 않은 경우는 컬럼 메타에 대한 처리만 한다.
+        # 실 데이터가 넘어오지 않은 경우는 컬럼 메타에 대한 처리만 한다.
+        if row:
             self.cells = row.findall("./{%s}Cell" % self.xmlns)
 
         # 헤더 컬럼 수와 실제 데이터 컬럼수가 같아질때까지 반복한다
@@ -23,17 +24,23 @@ class RowMapper:
 
         for i, header in enumerate(headers):
             header_meta = ColumnMapper(header, styles)
+            header_meta.preprocessing()
 
-            if row != None:
-                column_meta = ColumnMapper(self.cells[i], styles)
+            if row:
+                try:
+                    column_meta = ColumnMapper(self.cells[i], styles)
+                    column_meta.preprocessing()
 
-                self.columns[header_meta.getText()] = column_meta
+                    self.columns[header_meta.getText()] = column_meta
+                except IndexError:
+                    pass
 
     def __getitem__(self, column_name):
         if column_name in self.columns:
             return self.columns[column_name]
         else:
-            raise Exception("'%s'에 해당하는 컬럼이 없습니다.")
+            print(self.columns.keys())
+            raise Exception("'%s'에 해당하는 컬럼이 없습니다." % column_name)
 
     def keys(self):
         return self.columns
@@ -70,7 +77,7 @@ class RowMapper:
 
 
 class ColumnMapper:
-    def __init__(self, cell, style):
+    def __init__(self, cell, style_dict):
         self.namespaces = {
             'ns': 'urn:schemas-microsoft-com:office:spreadsheet',
             'ss': 'urn:schemas-microsoft-com:office:spreadsheet'
@@ -78,26 +85,34 @@ class ColumnMapper:
 
         # 특정 걸럼이 날짜 데이터 타입인지 저장해둔다.
         self.is_date = False
+        self.styles = style_dict
+        self.cell = cell
+        self.cell_data = None
+        self.style_data = None
 
+    def preprocessing(self):
         # styleID 속성값을 가져온다. 없는 경우 None을 반환한다.
-        cell_style_id = cell.get("{%s}StyleID" % self.namespaces['ss'])
-        if cell_style_id != None and style != None:
-            self.style = style[cell_style_id]
+        cell_style_id = self.cell.get("{%s}StyleID" % self.namespaces['ss'])
+
+        if cell_style_id and self.styles:
+            self.style_data = self.styles[cell_style_id]
 
         # 실 데이터를 가져온다(String과 DateTime)
-        cell_data = cell.find("./{%s}Data" % self.namespaces['ns'])
+        cell_data = list(self.cell)
+        cell_data = cell_data[0] if len(cell_data) > 0 else Element('{%s}Data' % self.namespaces['ns'])
 
-        # Type 속성값을 가져온다 없는 경우 None을 반환한다
-        cell_data_type = cell_data.get("{%s}Type" % self.namespaces['ss'])
-        if cell_data_type == "DateTime":
-            self.is_date = True
+        if cell_data.tag:
+            # Type 속성값을 가져온다 없는 경우 None을 반환한다
+            cell_data_type = cell_data.get("{%s}Type" % self.namespaces['ss'])
+            if cell_data_type == "DateTime":
+                self.is_date = True
 
-        if cell_data != None:
             if self.is_date:
                 self.cell_data = datetime.datetime.strptime(cell_data.text, "%Y-%m-%dT%H:%M:%S.%f")
             else:
                 self.cell_data = cell_data.text
         else:
+
             self.cell_data = ""
 
     def getText(self):
